70f36467e76ff618c071385005ff656d243c3dea
mfmoraes <melchior.alencar@gmail.com>
2020-03-31 06:15:08 -0500

    Usando Pipes

--- Angular/first-angular-cli/src/app/app.component.ts
+++ Angular/first-angular-cli/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { User } from './user';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,5 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tasks';
+  upperText:string = 'display uppercase text';
+  lowerText:string = 'Display lowcerase text';
+  dateValue: Date = new Date();
+  money: number = 562;
+  percentValue:number = 0.5;
+  user:  User = {
+    name: 'John',
+    age: 36
+  }
   
 }
