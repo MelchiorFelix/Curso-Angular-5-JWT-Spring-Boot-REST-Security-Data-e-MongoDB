9f1c8c7ca29be5fc6e1055149c8e26e9a95ee56e
mfmoraes <melchior.alencar@gmail.com>
2020-04-06 07:53:53 -0500

    Spring Security

--- Spring Boot/firstSpringBoot/src/main/java/com/melchior/fisrtspringboot/controller/FirstSpringBootController.java
+++ Spring Boot/firstSpringBoot/src/main/java/com/melchior/fisrtspringboot/controller/FirstSpringBootController.java
@@ -1,10 +1,15 @@
-package com.example.demo.controller;
+package com.melchior.fisrtspringboot.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FirstSpringBootController {
+	
+	@GetMapping("/")
+	public String index() {
+		return "index page";
+	}
 	
 	@GetMapping("/showtext")
 	public String showText() {
