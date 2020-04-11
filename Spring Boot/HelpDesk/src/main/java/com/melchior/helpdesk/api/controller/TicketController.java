807ec53fce76d1dbdfca48b38dc7cb3c001fd5d6
mfmoraes <melchior.alencar@gmail.com>
2020-04-10 22:48:01 -0500

    Criando o Controller de Ticket Parte 4

--- Spring Boot/HelpDesk/src/main/java/com/melchior/helpdesk/api/controller/TicketController.java
+++ Spring Boot/HelpDesk/src/main/java/com/melchior/helpdesk/api/controller/TicketController.java
@@ -24,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.melchior.helpdesk.api.dto.Summary;
 import com.melchior.helpdesk.api.response.Response;
 import com.melchior.helpdesk.api.security.entity.ChangeStatus;
 import com.melchior.helpdesk.api.security.entity.Ticket;
@@ -212,8 +213,94 @@ public class TicketController {
 			return ResponseEntity.ok(response);
 	}
 	
+	@PutMapping(value = "{id}/{status}")
+	@PreAuthorize("hasAnyRole('CUSTOMER', 'TECHNICIAN')")
+	public ResponseEntity<Response<Ticket>> changeStatus(@PathVariable("id") String id, @PathVariable("status") String status,
+			HttpServletRequest request, @RequestBody Ticket ticket, BindingResult result){
+		Response<Ticket> response = new Response<Ticket>();
+		try {
+			validateChangeStatus(id, status, result);
+			if(result.hasErrors()) {
+				result.getAllErrors().forEach(error -> response.getErrors().add(error.getDefaultMessage()));
+				return ResponseEntity.badRequest().body(response);
+			}
+			Ticket ticketCurrent = ticketService.findById(id);
+			ticketCurrent.setStatus(StatusEnum.getStatus(status));
+			if(status.equals("Assigned")) {
+				ticketCurrent.setAssignedUser(userFromRequest(request));
+			}
+			Ticket ticketPersisted = (Ticket) ticketService.createOrUpdate(ticketCurrent);
+			ChangeStatus changeStatus = new ChangeStatus();
+			changeStatus.setUserChange(userFromRequest(request));
+			changeStatus.setDateChangeStatus(new Date());
+			changeStatus.setStatus(StatusEnum.getStatus(status));
+			changeStatus.setTicket(ticketPersisted);
+			ticketService.createChangeStatus(changeStatus);
+			response.setData(ticketPersisted);
+		} catch (Exception e) {
+			response.getErrors().add(e.getMessage());
+			return ResponseEntity.badRequest().body(response);
+		}
+		return ResponseEntity.ok(response);
+	}
 	
 	
+	private void validateChangeStatus(String id,String status, BindingResult result) {
+		if (id == null || id.equals("")) {
+			result.addError(new ObjectError("Ticket", "Id no informed"));
+			return;
+		}
+		if (status == null || status.equals("")) {
+			result.addError(new ObjectError("Ticket", "Status no informed"));
+			return;
+		}
+	}
+	
+	@GetMapping(value = "/summary")
+	public ResponseEntity<Response<Summary>> findSummary(){
+		Response<Summary> response = new Response<Summary>();
+		Summary summary = new Summary();
+		int amountNew = 0;
+		int amountResolved = 0;
+		int amountApproved = 0;
+		int amountDisapproved = 0;
+		int amountAssigned = 0;
+		int amountClosed = 0;
+		
+		Iterable<Ticket> tickets = ticketService.findAll();
+		if(tickets != null) {
+			for (Iterator<Ticket> iterator = tickets.iterator(); iterator.hasNext();) {
+				Ticket ticket = (Ticket) iterator.next();
+				if(ticket.getStatus().equals(StatusEnum.New)) {
+					amountNew++;
+				}
+				if(ticket.getStatus().equals(StatusEnum.Resolved)) {
+					amountResolved++;
+				}
+				if(ticket.getStatus().equals(StatusEnum.Approved)) {
+					amountApproved++;
+				}
+				if(ticket.getStatus().equals(StatusEnum.Dissapproved)) {
+					amountDisapproved++;
+				}
+				if(ticket.getStatus().equals(StatusEnum.Assigned)) {
+					amountAssigned++;
+				}
+				if(ticket.getStatus().equals(StatusEnum.Closed)) {
+					amountClosed++;
+				}
+			}
+		}
+		summary.setAmountNew(amountNew);
+		summary.setAmountApproved(amountApproved);
+		summary.setAmountResolved(amountResolved);
+		summary.setAmountApproved(amountApproved);
+		summary.setAmountDisapproved(amountDisapproved);
+		summary.setAmountAssigned(amountAssigned);
+		summary.setAmountClosed(amountClosed);
+		response.setData(summary);
+		return ResponseEntity.ok(response);
+	}
 	
 	
 	
