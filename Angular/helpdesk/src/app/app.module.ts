d32ed7e659571827158365933db0313119ebadf3
mfmoraes <melchior.alencar@gmail.com>
2020-04-14 20:49:20 -0500

    HTTP Interceptors e Route Guard

--- Angular/helpdesk/src/app/app.module.ts
+++ Angular/helpdesk/src/app/app.module.ts
@@ -11,8 +11,10 @@ import { LoginComponent } from './components/security/login/login.component';
 import { routes } from './app.routes';
 import { UserService } from './services/user.service';
 import { SharedService } from './services/shared.service';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { FormsModule } from '@angular/forms'
+import { AuthInterceptor } from './components/security/auth.interceptor';
+import { AuthGuard } from './components/security/auth.guard';
 
 
 @NgModule({
@@ -29,8 +31,18 @@ import { FormsModule } from '@angular/forms'
     FormsModule,
     HttpClientModule,
     routes
+    
   ],
-  providers: [UserService, SharedService],
+  providers: [
+    UserService, 
+    SharedService,
+    AuthGuard,
+    {
+      provide : HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Angular/helpdesk/src/app/app.routes.ts
+++ Angular/helpdesk/src/app/app.routes.ts
@@ -2,12 +2,13 @@ import { Routes, RouterModule } from '@angular/router'
 import { HomeComponent } from './components/home/home.component'
 import { LoginComponent } from './components/security/login/login.component'
 import { ModuleWithProviders } from '@angular/core'
+import { AuthGuard } from './components/security/auth.guard'
 
 
 
 
 export const ROUTES: Routes = [
-    { path: '', component: HomeComponent},
+    { path: '', component: HomeComponent, canActivate: [AuthGuard]},
     { path: 'login', component : LoginComponent}
 ]
 
