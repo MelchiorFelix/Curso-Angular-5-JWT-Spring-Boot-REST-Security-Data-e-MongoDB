c1898b2773237ef9ffb47a12bd8775ec30f8767d
mfmoraes <melchior.alencar@gmail.com>
2020-04-14 21:59:38 -0500

    Criando Componente UserNew Parte 02

--- Angular/helpdesk/src/app/app.routes.ts
+++ Angular/helpdesk/src/app/app.routes.ts
@@ -1,15 +1,17 @@
 import { Routes, RouterModule } from '@angular/router'
 import { HomeComponent } from './components/home/home.component'
 import { LoginComponent } from './components/security/login/login.component'
-import { ModuleWithProviders } from '@angular/core'
+import { ModuleWithProviders, Component } from '@angular/core'
 import { AuthGuard } from './components/security/auth.guard'
+import { UserNewComponent } from './components/user-new/user-new.component'
 
 
 
 
 export const ROUTES: Routes = [
     { path: '', component: HomeComponent, canActivate: [AuthGuard]},
-    { path: 'login', component : LoginComponent}
+    { path: 'login', component : LoginComponent},
+    { path: 'user-new', component : UserNewComponent, canActivate: [AuthGuard]}
 ]
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(ROUTES);
