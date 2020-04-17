3a5dd95006dec32c7b0e2726a81b2105ec9a66b4
mfmoraes <melchior.alencar@gmail.com>
2020-04-16 19:03:00 -0500

    Criando Componente TicketNew Parte 01

--- Angular/helpdesk/src/app/app.routes.ts
+++ Angular/helpdesk/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { ModuleWithProviders, Component } from '@angular/core'
 import { AuthGuard } from './components/security/auth.guard'
 import { UserNewComponent } from './components/user-new/user-new.component'
 import { UserListComponent } from './components/user-list/user-list.component'
+import { TicketNewComponent } from './components/ticket-new/ticket-new.component'
 
 
 
@@ -14,7 +15,8 @@ export const ROUTES: Routes = [
     { path: 'login', component : LoginComponent},
     { path: 'user-new', component : UserNewComponent, canActivate: [AuthGuard]},
     { path: 'user-new/:id', component : UserNewComponent, canActivate: [AuthGuard]},
-    { path: 'user-list', component : UserListComponent, canActivate: [AuthGuard]}
+    { path: 'user-list', component : UserListComponent, canActivate: [AuthGuard]},
+    { path: 'ticket-new', component : TicketNewComponent, canActivate: [AuthGuard]}
 ]
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(ROUTES);
