090c23a8596e3e707cfd40f27d747572a9a99731
mfmoraes <melchior.alencar@gmail.com>
2020-04-18 11:20:42 -0500

    Criando Componente TicketList Parte 02

--- Angular/helpdesk/src/app/app.routes.ts
+++ Angular/helpdesk/src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { AuthGuard } from './components/security/auth.guard'
 import { UserNewComponent } from './components/user-new/user-new.component'
 import { UserListComponent } from './components/user-list/user-list.component'
 import { TicketNewComponent } from './components/ticket-new/ticket-new.component'
+import { TicketListComponent } from './components/ticket-list/ticket-list.component'
 
 
 
@@ -16,6 +17,7 @@ export const ROUTES: Routes = [
     { path: 'user-new', component : UserNewComponent, canActivate: [AuthGuard]},
     { path: 'user-new/:id', component : UserNewComponent, canActivate: [AuthGuard]},
     { path: 'user-list', component : UserListComponent, canActivate: [AuthGuard]},
+    { path: 'ticket-list', component : TicketListComponent, canActivate: [AuthGuard]},
     { path: 'ticket-new', component : TicketNewComponent, canActivate: [AuthGuard]},
     { path: 'ticket-new/:id', component : TicketNewComponent, canActivate: [AuthGuard]}
 ]
