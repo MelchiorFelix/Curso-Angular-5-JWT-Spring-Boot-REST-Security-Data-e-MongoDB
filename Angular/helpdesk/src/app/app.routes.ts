97ebf4c30f99398893a72de3030708d791a43bba
mfmoraes <melchior.alencar@gmail.com>
2020-04-19 22:02:16 -0500

    Criando Componente Summary

--- Angular/helpdesk/src/app/app.routes.ts
+++ Angular/helpdesk/src/app/app.routes.ts
@@ -8,6 +8,7 @@ import { UserListComponent } from './components/user-list/user-list.component'
 import { TicketNewComponent } from './components/ticket-new/ticket-new.component'
 import { TicketListComponent } from './components/ticket-list/ticket-list.component'
 import { TicketDetailComponent } from './components/ticket-detail/ticket-detail.component'
+import { SummaryComponent } from './components/summary/summary.component'
 
 
 
@@ -21,7 +22,8 @@ export const ROUTES: Routes = [
     { path: 'ticket-list', component : TicketListComponent, canActivate: [AuthGuard]},
     { path: 'ticket-new', component : TicketNewComponent, canActivate: [AuthGuard]},
     { path: 'ticket-new/:id', component : TicketNewComponent, canActivate: [AuthGuard]},
-    { path: 'ticket-detail/:id', component : TicketDetailComponent, canActivate: [AuthGuard]}
+    { path: 'ticket-detail/:id', component : TicketDetailComponent, canActivate: [AuthGuard]},
+    { path: 'summary', component : SummaryComponent, canActivate: [AuthGuard]}
 ]
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(ROUTES);
